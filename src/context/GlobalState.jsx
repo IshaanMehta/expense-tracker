0202ea33ae10e6bbf05edb3a36ce35234785c636
IshaanMehta <ishaan2u@gmail.com>
2022-02-16 21:42:47 +0530

    setup context and reducer for global state mang.

---
+++ src/context/GlobalState.jsx
@@ -0,0 +1,31 @@
+import React, { useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+const initialState = {
+  transactions: [
+    { id: 1, text: "Flower", amount: -20 },
+    { id: 2, text: "Salary", amount: 300 },
+    { id: 3, text: "Book", amount: -10 },
+    { id: 4, text: "Camera", amount: 150 }
+  ]
+};
+
+// Global context
+
+export const GlobalContext = React.createContext(initialState);
+
+// Provider component
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
