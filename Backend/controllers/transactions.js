d573ea525617d0e20d75560fb6bf3b4fe302cab4
IshaanMehta <ishaan2u@gmail.com>
2022-02-20 18:48:18 +0530

    database setup & api connectivity

--- Backend/controllers/transactions.js
+++ Backend/controllers/transactions.js
@@ -1,23 +1,81 @@
+const Transaction = require("../models/Transaction");
+
 // @desc get all transactions
 // @routes GET /api/v1/transactions
 // acess Pubic
 
-exports.getTransactions = (req, res, next) => {
-  res.send("GET Transactions");
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error"
+    });
+  }
 };
 
 // @desc add transaction
 // @routes POST /api/v1/transactions
 // acess Pubic
 
-exports.addTransaction = (req, res, next) => {
-  res.send("ADD Transaction");
+exports.addTransaction = async (req, res, next) => {
+  try {
+    const { text, amount } = req.body;
+    const transaction = await Transaction.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      data: transaction
+    });
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      const messages = Object.values(err.errors).map((val) => val.message);
+
+      return res.status(400).json({
+        success: false,
+        error: messages
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: "Server Error"
+      });
+    }
+  }
 };
 
 // @desc delete transaction
 // @routes DELETE /api/v1/transactions/:id
 // acess Pubic
 
-exports.deleteTransaction = (req, res, next) => {
-  res.send("DELETE Transaction");
+exports.deleteTransaction = async (req, res, next) => {
+  try {
+    const transaction = await Transaction.findById(req.params.id);
+
+    if (!transaction) {
+      return res.status(404).json({
+        success: true,
+        error: "No Transaction Found"
+      });
+    }
+
+    await transaction.remove();
+
+    return res.status(200).json({
+      success: true,
+      data: {}
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error"
+    });
+  }
 };
